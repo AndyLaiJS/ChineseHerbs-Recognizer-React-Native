4b92cdfe0ed22fea27c0e3d98e20236352cd0b09
AndyPhoenix9879 <lai9879m@gmail.com>
2022-02-13 01:18:38 +0800

    There is issues on these files with fetch()

--- android/app/src/debug/java/com/fypmobile/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/fypmobile/ReactNativeFlipper.java
@@ -40,7 +40,8 @@ public class ReactNativeFlipper {
           new NetworkingModule.CustomClientBuilder() {
             @Override
             public void apply(OkHttpClient.Builder builder) {
-              builder.addNetworkInterceptor(new FlipperOkhttpInterceptor(networkFlipperPlugin));
+              // builder.addNetworkInterceptor(new
+              // FlipperOkhttpInterceptor(networkFlipperPlugin));
             }
           });
       client.addPlugin(networkFlipperPlugin);
